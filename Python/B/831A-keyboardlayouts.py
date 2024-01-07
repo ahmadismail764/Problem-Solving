ed90dd7c152d1ce072bfcbe1c76cca1afb278d41
Ahmad Ismail <ahmedeldeehy30305@gmail.com>
2024-01-07 10:00:28 +0200

    Very slight modification

--- Python/B/831A-keyboardlayouts.py
+++ Python/B/831A-keyboardlayouts.py
@@ -11,8 +11,11 @@ class Solver:
         new_text = ""
         for c in self.text:
             if c.isalpha():
-                new_char = self.layout2[self.layout1.find(c.lower())].upper(
-                ) if c.isupper() else self.layout2[self.layout1.find(c)]
+                new_char = (
+                    self.layout2[self.layout1.find(c.lower())].upper()
+                    if c.isupper()
+                    else self.layout2[self.layout1.find(c)]
+                )
                 new_text += new_char
             else:
                 new_text += c
