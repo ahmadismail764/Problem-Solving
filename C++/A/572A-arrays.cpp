2dabc86fbe47a1ecfc22be97652535ddcbcd9d3f
Ahmad Ismail <ahmedeldeehy30305@gmail.com>
2024-01-12 19:54:15 +0200

    Arrays

---
+++ C++/A/572A-arrays.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int na, nb, k, m;
+    cin >> na >> nb >> k >> m;
+    vector<int> a(na), b(nb);
+    for (int i = 0; i < na; i++)
+        cin >> a[i];
+    for (int i = 0; i < nb; i++)
+        cin >> b[i];
+    cout << ((a[k - 1] < b[nb - m]) ? "YES" : "NO");
+}
