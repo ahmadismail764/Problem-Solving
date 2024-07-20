72c3349a2973374ef8de73b4d02ccdb63005b081
Ahmad Ismail <ahmedeldeehy30305@gmail.com>
2024-07-20 17:57:17 +0300

    Counts? I don't remember if I did this one on my own at first or not

---
+++ Sheets/Prefix/E-fence.py
@@ -0,0 +1,13 @@
+n, k = map(int, input().split())
+arr = list(map(int, input().split()))
+prefix = [0] * (n+1)
+for i in range(1, n+1):
+    prefix[i] = prefix[i-1] + arr[i-1]
+minimum = float('inf')
+answer = 0
+for i in range(k, n+1):
+    curr_diff = prefix[i] - prefix[i-k] 
+    if curr_diff < minimum:
+        answer = i - k + 1
+        minimum = curr_diff
+print(answer)
