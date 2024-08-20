72c31ad524ab67dfe1dbbd8f9dce1e64613b3b2c
ahmadismail764 <ahmedeldeehy30305@gmail.com>
2024-08-20 08:09:28 +0300

    Original Solution, some AI assistance, but still original solution

--- Sheets/Binary Search/C-subarraysumsI.py
+++ Sheets/Binary Search/C-subarraysumsI.py
@@ -1,24 +1,13 @@
 n, x = map(int, input().split())
 arr = list(map(int, input().split()))
-prefix = [0] * (n+1)
-for i in range(1, n+1):
-    prefix[i] = prefix[i-1] + arr[i-1]
-if prefix[n] <= x:
-    print(int(prefix[n] == x))
-else:
-    start, end, k_index = 1, n, -1
-    while start <= end:
-        mid = (start + end) // 2
-        if prefix[mid] >= x:
-            k_index = mid
-            end = mid - 1
-        else:
-            start = mid + 1
-    if k_index == - 1:
-        print(0)
-    else:
-        ans = int(prefix[k_index] == x)
-        for i in range(k_index+1, n+1):
-            curr = abs(x - prefix[i])
-            ans += prefix[:i].count(curr)
-        print(ans)
+prefix_count = {}
+curr_sum = 0
+prefix_count[0] = 1
+for a in arr:
+    curr_sum += a
+    prefix_count[curr_sum] = prefix_count.get(curr_sum, 0) + 1
+ans = 0
+for key, value in prefix_count.items():
+    if key >= x:
+        ans += prefix_count.get(key-x, 0)
+print(ans)
