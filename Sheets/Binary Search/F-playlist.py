78e83d6e6414eef9c91487a97125aa3317e60306
ahmadismail764 <ahmedeldeehy30305@gmail.com>
2024-08-28 10:01:04 +0300

    AI Solution

--- Sheets/Binary Search/F-playlist.py
+++ Sheets/Binary Search/F-playlist.py
@@ -1,2 +1,11 @@
 n = int(input())
 arr = list(map(int, input().split()))
+unique_elements = set()
+start, max_len = 0, 0
+for end in range(n):
+    while arr[end] in unique_elements:
+        unique_elements.remove(arr[start])
+        start += 1
+    unique_elements.add(arr[end])
+    max_len = max(max_len, end - start + 1)
+print(max_len)
