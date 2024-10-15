998a6df55a518262d82d7f5d45d5d063594d1956
ahmadismail764 <ahmedeldeehy30305@gmail.com>
2024-10-15 09:38:14 +0300

    File management

--- Sheets/Contest 1/_D-infinity.py
+++
@@ -1,8 +0,0 @@
-n = int(input())
-arr = []
-for _ in range(n):
-    arr.append(tuple(map(int, input().split())))
-m = int(input())
-arr_m = list(map(int, input().split()))
-for m in arr_m:
-    print(sum((m * a >= b) for a, b in arr))
